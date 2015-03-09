693230f7302dbfffb0494e2e39897a56e4bc931a
unknown <madhab@Arjun-bhatta.f1soft.com>
2015-03-09 10:37:28 +0545

    comfigured userbundle with fos user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,12 +17,13 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\RestBundle\FOSRestBundle(),
-            // new FOS\UserBundle\FOSUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new Rest\ApiBundle\RestApiBundle(),
             new Rest\AuthBundle\RestAuthBundle(),
+            new Rest\UserBundle\RestUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
