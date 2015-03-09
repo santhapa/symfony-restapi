8e39ac94eaf106724937a596462de66987e935eb
unknown <madhab@Arjun-bhatta.f1soft.com>
2015-03-09 10:38:12 +0545

    added entity classes for auth bundle

---
+++ src/Rest/AuthBundle/Entity/AccessToken.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Rest\AuthBundle\Entity;
+
+use FOS\OAuthServerBundle\Entity\AccessToken as BaseAccessToken;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="tbl_access_token")
+ * @ORM\Entity
+ */
+class AccessToken extends BaseAccessToken
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Client")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    protected $client;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Rest\UserBundle\Entity\User")
+     */
+    protected $user;
+}
